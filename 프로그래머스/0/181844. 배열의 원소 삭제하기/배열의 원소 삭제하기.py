ce2802a9a19472b12568fc02d56e221f0ad7f1d9
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-11 21:40:32 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 0.14 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.py
@@ -1,3 +1,6 @@
+# 내가 한 풀이
+# 메모리: 10.1 MB, 시간: 0.35 ms
+'''
 def solution(arr, delete_list):
     answer = []
     for i in delete_list:
@@ -5,3 +8,9 @@ def solution(arr, delete_list):
             if i==j:
                 arr.remove(j)
     return arr
+'''
+
+# 조건부 표현식, 리스트 컴프리헨션, 포함, 불포함을 확인할 수 있는 in, not in 연산 사용
+
+def solution(arr, delete_list):
+    return [i for i in arr if i not in delete_list]
