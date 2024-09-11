8ba822d4280cbd1d2ecf50e0314c89af9ed1ff06
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-11 18:00:58 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,2 +1,10 @@
+# 내가 한 풀이-라이브러리사용
+# 메모리: 10.3 MB, 시간: 0.00 ms
+'''
 def solution(my_string, is_suffix):
     return int(my_string.endswith(is_suffix))
+'''
+
+# 남이 한 풀이
+def solution(my_string, is_suffix):
+    return int(my_string[-len(is_suffix):]==is_suffix)
