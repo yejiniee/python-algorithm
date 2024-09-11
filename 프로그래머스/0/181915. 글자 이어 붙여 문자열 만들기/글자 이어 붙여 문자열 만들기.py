2c74ead858a52f6bd17e5a3f4dd8a15dee99df0f
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-11 18:22:30 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.14 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.py
@@ -1,5 +1,16 @@
+#내가 한 풀이
+
 def solution(my_string, index_list):
     answer = ''
-    for i in range(len(index_list)):
-        answer+=my_string[index_list[i]]
+    for i in index_list:
+        answer+=my_string[i]
     return answer
+
+'''
+# 리스트 컴프리헨션 사용
+def solution(my_string, index_list):
+    return ''.join([my_string[idx] for idx in index_list])
+
+# 리스트 컴프리헨션을 통해 ["p", "i", "z", "z", "a"] 리스트 생성
+# join 메서드를 이용해 리스트의 각 문자를 연결하여 하나의 문자열로 만듦
+'''
