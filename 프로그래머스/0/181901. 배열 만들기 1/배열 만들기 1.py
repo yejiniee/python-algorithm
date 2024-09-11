6d6cebd11f7082ddcaabfa0bd69f3463786c5ba8
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-11 17:44:29 +0900

    [level 0] Title: 배열 만들기 1, Time: 0.14 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,3 +1,10 @@
+# 리스트 컴프리헨션 사용
+def solution(n, k):
+    return [i for i in range(k,n+1,k)]
+
+# 내가 한 방식
+# 메모리: 10.5 MB, 시간: 0.62 ms
+'''
 def solution(n, k): 
     answer = []
     i=1
@@ -6,4 +13,5 @@ def solution(n, k):
         i+=1; 
         
     return answer
+'''
 
