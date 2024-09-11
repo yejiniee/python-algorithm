8037bca7add1a3b4d6df2d2c60b88e27865fd605
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-11 21:17:42 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
@@ -1,4 +1,14 @@
+# 내가 한 풀이
+# 메모리: 10.3 MB, 시간: 0.03 ms
+'''
 def solution(a, b):
     addStr=str(a)+str(b)
     mul=2*a*b
     return (int(addStr) if int(addStr)>=mul else mul) 
+'''
+
+
+# 다른 사람의 풀이-max 함수 사용
+def solution(a, b):
+    return max(int(str(a) + str(b)), 2 * a * b)
+# 만약 둘의 값이 같은 경우 앞의 수를 리턴함
