a983dbdee95615433705f59dbc88d43af559953e
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-11 21:25:48 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.py
@@ -1,4 +1,11 @@
+# 내가 한 풀이
+# 메모리: 10.3 MB, 시간: 0.03 ms
+'''
 def solution(a, b):
     aToB=int(str(a)+str(b))
     bToA=int(str(b)+str(a))
     return max(aToB, bToA)
+'''
+# 다른 사람의 풀이- 포맷스트링 사용
+def solution(a, b):
+    return max(int(f"{a}{b}"), int(f"{b}{a}"))
