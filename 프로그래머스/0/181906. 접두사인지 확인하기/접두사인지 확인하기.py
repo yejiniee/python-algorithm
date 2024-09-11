9ee186c21c581c544cd33606f5595994388613e1
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-11 17:50:51 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,15 +1,16 @@
 # 파이썬 라이브러리 startswith 사용
+# 메모리: 10.1 MB, 시간: 0.00 ms -> 라이브러리 사용이 메모리 up
+'''
 def solution(my_string, is_prefix):
     return int(my_string.startswith(is_prefix))
+'''
 
 # 내가 한 풀이
 # 메모리: 10 MB, 시간: 0.00 ms
-'''
 def solution(my_string, is_prefix):
     if(my_string[0:len(is_prefix)]==is_prefix):
         return 1
     else:
         return 0
-'''
     
     
