9f3b761ad8cf4891989c1384cfba663b7a338870
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-11 21:00:05 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -1,3 +1,6 @@
+# 내가 한 풀이
+# 메모리: 10.2 MB, 시간: 0.00 ms
+'''
 def solution(num_list):
     if (num_list[-1]>num_list[-2]):
         num_list.append(num_list[-1]-num_list[-2])
@@ -5,4 +8,9 @@ def solution(num_list):
         num_list.append(num_list[-1]*2)
     
     return num_list
+'''
 
+# 다른 사람의 풀이
+def solution(num_list):
+    num_list.append(num_list[-1]-num_list[-2] if num_list[-1]>num_list[-2] else num_list[-1]*2)
+    return num_list
