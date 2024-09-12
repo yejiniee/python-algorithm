3d5d121b501f03b521aa027d5309a018208f67fb
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-12 18:39:59 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,5 +1,13 @@
+# 내가 한 풀이
+# 메모리: 10.3 MB, 시간: 0.01 ms
+'''
 def solution(myString, pat):
     newString = ''.join(['B' if s=='A' else 'A' for s in myString])
     
     return int(pat in newString)
+'''
 
+
+# 다른 사람의 풀이- replace 메서드 사용
+def solution(myString, pat):
+    return int(pat in myString.replace('A', 'C').replace('B', 'A').replace('C', 'B'))
