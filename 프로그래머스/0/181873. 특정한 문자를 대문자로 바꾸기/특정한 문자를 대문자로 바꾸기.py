1c2ce657eb73a4636c945f64f11035c6009ba97e
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-12 20:14:50 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.py
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.py
@@ -1,4 +1,11 @@
+# 내가 한 풀이
+# 메모리: 10.1 MB, 시간: 0.11 ms
+'''
 def solution(my_string, alp):
     return ''.join([i.upper() if i==alp else i for i in my_string])
+'''
 
+# 다른 사람의 풀이
 
+def solution(my_string, alp):
+    return my_string.replace(alp, alp.upper())
