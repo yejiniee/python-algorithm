9d9477c33b9bee28338785ac04127bb11a50b858
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-12 16:13:02 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 9.91 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.py
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.py
@@ -1,2 +1,11 @@
+# 내가 한 풀이 - 리스트 컴프리헨션 사용
+# 메모리: 10.2 MB, 시간: 0.01 ms
+'''
 def solution(start_num, end_num):
     return [i for i in range(start_num, end_num+1)]
+'''
+
+
+#  다른 사람의 풀이 - 리스트 내장함수 사용
+def solution(start, end):
+    return list(range(start, end + 1))
