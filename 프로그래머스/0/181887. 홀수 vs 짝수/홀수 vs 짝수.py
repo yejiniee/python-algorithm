7306aa8e98141aeccc6f9b39e2992cfa8144e148
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-12 17:28:49 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -1,5 +1,14 @@
+# 내가 한 풀이
+# 메모리: 10.1 MB, 시간: 0.01 ms
+'''
 def solution(num_list):
     odd = [num_list[i] for i in range(0,len(num_list),2)]
     even = [num_list[i] for i in range(1,len(num_list),2)]
     
     return max(sum(odd), sum(even)) 
+'''
+
+# 남이 한 풀이- 리스트 슬라이싱 사용
+# [::2]는 리스트의 처음부터 끝까지 특정 간격(2)으로 선택함-인덱스 짝수
+def solution(num_list):
+    return max(sum(num_list[::2]), sum(num_list[1::2]))
