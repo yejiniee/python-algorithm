7f662280ed9792c43c0b0728bd2eeb607eabe2cb
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-12 16:25:12 +0900

    [level 0] Title: 접미사 배열, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.py
@@ -1,6 +1,14 @@
+# 내가 한 풀이
+# 메모리: 10.3 MB, 시간: 2.14 ms
+
 def solution(my_string):
-    answer = []
-    for i in my_string:
-        answer = [my_string[i:len(my_string)] for i in range(len(my_string))]
+    answer = [my_string[i:len(my_string)] for i in range(len(my_string))]
     answer.sort()
     return answer
+
+'''
+# 다른 사람의 풀이
+def solution(my_string):
+    return sorted(my_string[i:] for i in range(len(my_string)))
+    
+    '''
