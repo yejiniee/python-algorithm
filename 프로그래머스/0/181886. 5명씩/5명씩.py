f1ff20cf014211b8f4121dc0afe44d3379126fe2
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-12 20:06:35 +0900

    [level 0] Title: 5명씩, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.py
+++ 프로그래머스/0/181886. 5명씩/5명씩.py
@@ -1,2 +1,10 @@
+# 내가 한 풀이
+# 메모리: 10.3 MB, 시간: 0.00 ms
+'''
 def solution(names):
     return [names[n] for n in range(0,len(names),5)]
+'''
+
+# 다른 사람의 풀이
+def solution(names):
+    return names[::5]
