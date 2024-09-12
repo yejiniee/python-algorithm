40d2c0f6272626367462189bb24b1d49f236edbc
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-12 19:59:19 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
@@ -1,5 +1,7 @@
+# 내가 한 풀이
+# 메모리: 10.2 MB, 시간: 0.01 ms
+'''
 def solution(arr1, arr2):
-
     if len(arr1)>len(arr2):
         return 1
     elif len(arr1)==len(arr2):
@@ -10,4 +12,21 @@ def solution(arr1, arr2):
         return 0
     else:
         return -1
+'''
+
     
+# 남이 한 풀이
+
+def solution(arr1, arr2):
+    return (len(arr1) > len(arr2)) - (len(arr2) > len(arr1)) or (sum(arr1) > sum(arr2)) - (sum(arr2) > sum(arr1))
+
+# len(arr1) > len(arr2)) - (len(arr2) > len(arr1))
+# -> arr1의 길이가 arr2의 길이보다 크면 true(1)-false(0) => 'return 1'
+#    arr2의 길이가 arr1의 길이보다 크면 0 - 1 => 'return -1'
+#    같으면 0 - 0 => 'return 0'
+
+# (sum(arr1) > sum(arr2)) - (sum(arr2) > sum(arr1))
+# -> arr1의 합이 arr2의 합보다 크면 1 - 0 => 'return 1'
+#    arr2의 합이 arr1의 합보다 크면 0 - 1 => 'return -1'
+#    같으면 0 - 0 => 'return 0'
+
