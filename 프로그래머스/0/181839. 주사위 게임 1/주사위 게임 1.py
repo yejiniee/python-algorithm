32b393423603cde0f441c4dec5fa33766aa135b8
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-12 18:24:24 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.py
@@ -1,3 +1,6 @@
+# 내가 한 풀이
+# 메모리: 10.3 MB, 시간: 0.00 ms
+'''
 def solution(a, b):
     if (a%2!=0 and b%2!=0):
         return a**2 + b**2
@@ -8,6 +11,13 @@ def solution(a, b):
             return b-a
     else:
         return 2*(a+b)
-            
+'''
 
+# 다른 사람의 풀이- abs() 내장 함수 사용
+def solution(a, b):
+    if a%2 and b%2: 
+        return a*a+b*b
+    elif a%2 or b%2: 
+        return 2*(a+b)
+    return abs(a-b)        
 
