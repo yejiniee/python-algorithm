ab713e255c5659b61b17b789ae1a0fb96d9c5493
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-12 18:32:46 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.22 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.py
@@ -1,6 +1,16 @@
+# 내가 한 풀이
+# 메모리: 10.9 MB, 시간: 0.76 ms
+'''
 def solution(arr):
     new_arr = []
     for i in arr:
         for j in range(i):
             new_arr.append(i)
     return new_arr
+'''
+
+# 다른 사람의 풀이 - 리스트 컴프리헨션에 이중 for문 사용하기
+def solution(arr):
+    return [i for i in arr for j in range(i)]
+
+# 각 루프는 왼쪽에서 오른쪽으로 평가됨
