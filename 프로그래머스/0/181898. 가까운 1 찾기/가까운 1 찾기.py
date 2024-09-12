7e5bdcd4db1fd1c5559122ea7a7a0fa40cd324fa
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-12 21:27:18 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,6 +1,25 @@
+# 내가 푼 방식
+# 메모리: 10.3 MB, 시간: 0.00 ms
+'''
 def solution(arr, idx):
     for i in range(idx, len(arr)):
         if(arr[i]==1):
             return i
 
     return -1
+'''
+
+# 다른 사람의 풀이
+# try-catch 문과 index 메서드 사용
+
+def solution(arr, idx):
+    answer = 0
+    try:
+        answer = arr.index(1, idx)
+    except:
+        answer = -1 # 오류가 발생하면 -1을 반환
+
+    return answer
+
+# arr.index(1, idx) 
+# => 1이라는 값이 처음 나타나는 인덱스를 찾는 함수, 'idx'부터 탐색
