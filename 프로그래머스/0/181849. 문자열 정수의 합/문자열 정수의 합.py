934e480d072f88e2f56aff4d040959fe521ee203
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-12 18:02:28 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.py
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.py
@@ -1,4 +1,12 @@
+# 내가 한 풀이
+# 메모리: 10.3 MB, 시간: 0.04 ms
+'''
 def solution(num_str):
     new_list=list(num_str)
     
     return sum([int(s) for s in new_list])
+'''
+
+# 다른 사람의 풀이- 내장함수 map 사용
+def solution(num_str):
+    return sum(map(int, num_str))
