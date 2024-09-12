458ae6ed62fa6516cd764aec47e24f991b779b28
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-09-12 19:01:09 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.py
@@ -0,0 +1,13 @@
+def solution(arr1, arr2):
+
+    if len(arr1)>len(arr2):
+        return 1
+    elif len(arr1)==len(arr2):
+        if(sum(arr1)>sum(arr2)):
+            return 1
+        elif (sum(arr1)<sum(arr2)) :
+            return -1
+        return 0
+    else:
+        return -1
+    
