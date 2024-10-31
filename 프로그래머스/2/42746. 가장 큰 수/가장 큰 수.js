dca509f702df3787db2d4648af0993297ac43d9f
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-10-31 13:35:11 +0900

    Update 가장 큰 수.js

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -2,3 +2,14 @@ function solution(numbers) {
     let answer = numbers.sort((a, b) => `${b}${a}` - `${a}${b}`).join('');
     return answer[0] === '0' ? '0' : answer;
 }
+
+// 위의 코드가 더 성능적으로 비효율
+
+function solution(numbers) {
+    const answer = numbers
+        .map(number=>String(number))
+        .sort((a,b)=>(b+a)-(a+b))
+        .join('');
+    
+    return answer[0] === '0' ? '0' : answer;
+}
