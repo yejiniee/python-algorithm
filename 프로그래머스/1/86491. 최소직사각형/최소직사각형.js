9d9e7f914df335557ee3f1a559e249a220f4d160
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-11-01 14:53:22 +0900

    [level 1] Title: 최소직사각형, Time: 7.12 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,8 +1,8 @@
 function solution(sizes) {
     const sorted = sizes.map(innerArray => innerArray.sort((a, b) => b - a));
     
-    const x=Math.max(...sorted.map(arr=>arr[0]));
-    const y=Math.max(...sorted.map(arr=>arr[1]));
+    const w=Math.max(...sorted.map(arr=>arr[0]));
+    const h=Math.max(...sorted.map(arr=>arr[1]));
     
-    return x*y;
+    return w*h;
 }
