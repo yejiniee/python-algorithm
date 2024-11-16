239d45c4262ca1238aef81e239b5f8c0db11559c
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-11-16 13:42:58 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.71 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,22 +1,17 @@
 function solution(A,B){
-    let answer = 0;
     // 배열의 길이만큼 반복   
     // 최솟값이란? 가장큰 숫자랑 가장 작은 숫자랑 곱하기 
-  
  
     // 오름차순 / 내림차순 정렬
     A.sort((a, b)=>a-b);    
     B.sort((a, b)=>b-a);
     
-    let i=0;
-    while(i<A.length){
-        let sum=A[i]*B[i];
-        answer+=sum;
-        i++;
-    }
+   
     
+    // reduce - 누산기, 현재값, 인덱스, 배열
+    return A.reduce((acc, curr, idx, arr)=>
+        acc+=curr*B[idx]
+    , 0);
 
  
-
-    return answer;
 }
