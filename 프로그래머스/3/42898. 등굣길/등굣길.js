cf89f83ad822520e80bf3e8ff500f574abe8d41b
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2024-11-21 22:59:13 +0900

    [level 3] Title: 등굣길, Time: 3.37 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,19 +1,18 @@
 function solution(m, n, puddles) {
-    let dp=Array.from({length: n+1}, () => Array(m+1).fill(0));
-    
-    dp[1][1] = 1;
+    const dp = Array.from(Array(n), () => Array(m).fill(0));
+    puddles.forEach(([x,y]) => dp[y-1][x-1] = -1);
 
-    // puddles를 빠르게 확인하기 위해 Set 사용
-    const puddleSet = new Set(puddles.map(([x, y]) => `${x},${y}`));
-    
-    for(let i=1;i<=n;i++){
-        for(let j=1;j<=m;j++){
-            if(i === 1 && j === 1 || puddleSet.has(`${j},${i}`)) 
-                continue;
-            dp[i][j]=(dp[i][j-1]+dp[i-1][j])%1000000007;
+    dp[0][0] = 1;
+
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < m; j++){
+            let path = dp[i][j];
+            if(path === -1) continue;
+
+            if(i-1 >= 0 && dp[i-1][j] !== -1) path += dp[i-1][j];
+            if(j-1 >= 0 && dp[i][j-1] !== -1) path += dp[i][j-1];
+            dp[i][j] = path % 1000000007;
         }
     }
-    
-    
-    return dp[n][m];
+    return dp[n-1][m-1] % 1000000007;
 }
