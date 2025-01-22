ce5f9554ce2f00b24b4ae8fffbc0fe79bfc65d7b
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2025-01-22 18:49:45 +0900

    Update 조건에 맞는 개발자 찾기.sql

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,3 +1,5 @@
+-- SQL에서 비트 연산자는 숫자를 2진수로 자동 변환하여 연산.
+-- => 숫자를 직접 2진수로 변환할 필요 없이 비트 연산을 사용
 SELECT D.ID, D.EMAIL, D.FIRST_NAME, D.LAST_NAME
 FROM DEVELOPERS D
 -- SKILL_CODE의 비트 중에서 256 (Python에 해당하는 코드)을 포함하는지 확인
