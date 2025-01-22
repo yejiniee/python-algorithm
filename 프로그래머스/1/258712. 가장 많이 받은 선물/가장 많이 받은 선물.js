ba3f3b75fa3f757e89c9fb6ca25933756e38be85
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2025-01-22 18:23:44 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 34.32 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -0,0 +1,80 @@
+function solution(friends, gifts) {
+    const hashMap = new Map();
+    const friends_length=friends.length;
+    const nextGiftsCount=Array(friends_length).fill(0); // 다음 달 받을 선물 배열
+    const giftsScore=Array(friends_length).fill(0); // 선물지수 배열
+    
+    // 해시맵 저장하고 선물지수 저장함
+    for(let i=0 ; i< gifts.length ; i++){
+        if(!hashMap.has(gifts[i])){
+            // 없으면 추가
+            hashMap.set(gifts[i], 1);
+        }else{
+            // 있으면 +1
+            hashMap.set(gifts[i], hashMap.get(gifts[i]) + 1);
+        }
+        // 선물지수: 준 선물-받은 선물
+        giftsScore[friends.indexOf(gifts[i].split(' ')[0])]+=1; 
+        giftsScore[friends.indexOf(gifts[i].split(' ')[1])]-=1; 
+        
+        
+    }
+
+    
+    // 다음 달 받을 선물 수 저장
+    for(let i=0 ; i<friends_length-1 ; i++){
+        for(let j=i+1 ; j<friends_length; j++){
+            let a = friends[i];
+            let b = friends[j];
+            
+            let aToB = a+' '+b; 
+            let bToA = b+' '+a; 
+            
+            // 선물을 주고받은 경우
+            if(hashMap.has(aToB)||hashMap.has(bToA)){
+                const aToBValue=hashMap.get(aToB) ?? 0;
+                const bToAValue=hashMap.get(bToA) ?? 0;
+                
+                if(aToBValue>bToAValue) { // 2> 1
+                    
+                    // a의 다음달 선물 수 +1
+                    nextGiftsCount[i] += 1; 
+                } else if(aToBValue<bToAValue){
+                    // b의 다음달 선물 수 +1
+                    nextGiftsCount[j] += 1;
+                } else {
+                    // 선물 지수 비교
+                    if(giftsScore[i]>giftsScore[j]){
+                        // a의 다음달 선물 수 +1
+                        nextGiftsCount[i] += 1;
+                    }
+                    if(giftsScore[i]<giftsScore[j]){
+                        // b의 다음달 선물 수 +1
+                        nextGiftsCount[j] += 1;
+                    }
+                }
+            }else { // 선물을 주고받지 않은 경우
+                // 선물 지수 비교
+                if(giftsScore[i]>giftsScore[j]){
+                    // a의 다음달 선물 수 +1
+                    nextGiftsCount[i] += 1;
+                }
+                if(giftsScore[i]<giftsScore[j]){
+                    // b의 다음달 선물 수 +1
+                    nextGiftsCount[j] += 1; 
+                }
+            }
+            
+        }
+    }
+        
+    // giftScore에서 가장 큰 값을 찾아줘
+    answer=Math.max(...nextGiftsCount);
+    console.log('다음달 받을 선물 배열: ', nextGiftsCount);
+    console.log('선물지수 배열: ', giftsScore);
+    console.log('해시맵: ', hashMap);
+    
+    
+    
+    return answer; // 선물을 가장 많이 받을 친구의 받을 선물 수 
+}
