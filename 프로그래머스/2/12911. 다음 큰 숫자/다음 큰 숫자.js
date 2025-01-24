af39c93e964f58d864b7f9bec7d69e0205220ec6
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2025-01-24 15:59:46 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,19 @@
+function countNumberOfOne(n){
+        return n.toString(2).split('').filter((number)=>number==='1').length;
+    }
+
+function solution(n) {
+    // 1. n을 2진수 배열로 변환
+    // 2. numberOfOne: 1의 개수 세기
+    // 3. 반복문 78+i로 nextN에 1,2 적용해서 numberOfOne이랑 같으면 break하고 리턴
+    const numberOfOne=countNumberOfOne(n);
+    let i=1;
+    
+    while(true){
+        if(countNumberOfOne(n+i)===numberOfOne){
+            return n+i;
+        }
+        i++;
+    }
+}
+    
