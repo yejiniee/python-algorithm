715c50e2aedd56bb4c17dddc5b1adeb33a988263
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2025-01-24 15:24:17 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,3 +1,7 @@
+function hide_numbers(s){
+  return s.replace(/\d(?=\d{4})/g, "*");
+}
+
 function solution(phone_number) {
     var answer = '';
     const starLen = phone_number.length-4;
@@ -5,4 +9,14 @@ function solution(phone_number) {
     answer = starStr + phone_number.slice(starLen);
         
     return answer;
+}
+
+
+function hide_numbers(phone_number){
+    return phone_number.split('').fill('*', 0, phoneNumber.length - 4).join('');
+}
+
+function hide_numbers(s){
+    var result = "*".repeat(s.length - 4) + s.slice(-4);
+    return result;
 }
