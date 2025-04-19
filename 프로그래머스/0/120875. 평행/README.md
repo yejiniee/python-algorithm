9927cef57b6803f9fc488cb51994b90573eddc9f
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2025-04-19 16:07:36 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/README.md
+++ 프로그래머스/0/120875. 평행/README.md
@@ -1,10 +1,10 @@
 # [level 0] 평행 - 120875 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.14 MB, 시간: 0.01 ms
+메모리: 9.25 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 17일 21:05:03
+2025년 04월 19일 16:07:33
 
 ### 문제 설명
 
--- 프로그래머스/0/120875. 평행/평행.py
+++ 프로그래머스/0/120875. 평행/평행.py
@@ -1,11 +1,13 @@
 def solution(dots):
-    answer = 0
-    [[x1, y1], [x2, y2], [x3, y3], [x4, y4]] = dots
-    answer1 = (y2-y1)/(x2-x1) == (y4-y3)/(x4-x3)
-    answer2 = (y3-y1)/(x3-x1) == (y4-y2)/(x4-x2)        
-    answer3 = (y4-y1)/(x4-x1) == (y3-y2)/(x3-x2)
+
+    [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]=dots
+          
+    if (y2-y1)/(x2-x1) == (y4-y3)/(x4-x3):
+        return 1
+    if (y3-y1)/(x3-x1) == (y4-y2)/(x4-x2):
+        return 1
+    if (y4-y1)/(x4-x1) == (y3-y2)/(x3-x2):
+        return 1
     
-    if(answer1 or answer2 or answer3): 
-        answer=1
+    return 0
     
-    return answer
