69180e580ee1899815082ab7cadec5632e23afe8
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2025-07-29 16:33:54 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,13 +1,8 @@
 function solution(array, commands) {
-    const answer=[];
-    
-    for(let idx = 0; idx < commands.length ; idx++){
-        const [i,j,k] = commands[idx];
-        const newArray = array.slice(i-1,j); // i번째부터 j번째까지 슬라이스
-        newArray.sort((a,b)=>a-b); // 오름차순 정렬
-        answer.push(newArray[k-1]);
-    }
-    
-    
+    var answer = [];
+
+    answer = commands.map(a=>{
+        return array.slice(a[0]-1,a[1]).sort((b,c)=>b-c)[a[2]-1];
+    })
     return answer;
 }
