18d1471278f152cbb3f25e08b3b73dbf77cdb91c
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2025-07-29 17:01:03 +0900

    [level 2] Title: 가장 큰 수, Time: 185.20 ms, Memory: 44.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,10 +1,11 @@
 function solution(numbers) {
     // 만약 00000이면 0으로 출력
     
+    // 문자로 만들기
     // 내림차순으로 정렬
     // ba가 ab보다 크면(결과값 양수)=> ba 정렬
     // 결과값이 음수면 ab 정렬
-    let answer = numbers.sort((a,b)=>`${b}${a}`-`${a}${b}`).join('');
+    let answer=numbers.map(v=>v+'').sort((a,b)=>(b+a)-(a+b)).join('');
 
     return answer[0] === '0' ? '0' : answer;
 }
