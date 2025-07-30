1270196961524ecd548c3ce1bbc6b36c5a4c3907
Yejin Jo <80842764+yejiniee@users.noreply.github.com>
2025-07-30 17:27:37 +0900

    [level 3] Title: 여행경로, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,34 +1,29 @@
 function solution(tickets) {
-    // 객체로 저장
-    /*
-    {
-    'ICN': [JFK],
-    ...
+    const route = [];
+    const obj = {};
+
+    // 1. 그래프 만들기
+    tickets.forEach(([from, to]) => {
+        if (!obj[from]) obj[from] = [];
+        obj[from].push(to);
+    });
+
+    // 2. 알파벳 역순 정렬 (pop()으로 쓰기 위해)
+    for (let key in obj) {
+        obj[key].sort().reverse();
     }
-    
-    */
-    const route=[];
-    
-    const obj={};
-    
-    tickets.map(([a,b])=> {
-        if(!obj[a])
-            obj[a]=[];
-        obj[a].push(b);
-    })
-    for(let key in obj){
-        obj[key].sort().reverse(); // 내림차순 정렬
-    }
-    
-    function dfs(airport){
-        while(obj[airport] && obj[airport].length > 0){
-            const dest = obj[airport].pop();
-            dfs(dest);
+
+    // 3. DFS 정의
+    function dfs(airport) {
+        while (obj[airport] && obj[airport].length > 0) {
+            const next = obj[airport].pop();
+            dfs(next);
         }
         route.push(airport);
     }
-    
-    dfs('ICN');
-    
+
+    dfs("ICN");
+
+    // 4. 역순 결과 반환
     return route.reverse();
 }
